50e69ab487bc21fe43e18afba58dad19da79b735
yuqi_ryuu <yuqi_ryuu@sina.com>
2018-09-27 10:35:55 +0800

    add xep0004

---
+++ module/form/fields.go
@@ -0,0 +1,129 @@
+package form
+
+import (
+	"bytes"
+)
+
+type Fields struct {
+	fields      []*Field
+	fieldsByVar map[string]*Field
+}
+
+func NewFields() *Fields {
+	fields := &Fields{
+		fields:      nil,
+		fieldsByVar: make(map[string]*Field),
+	}
+
+	return fields
+}
+
+func (fs *Fields) AddField(field *Field) {
+	if field == nil {
+		return
+	}
+
+	cf, ok := fs.fieldsByVar[field.Var()]
+	if ok {
+		filtered := fs.fields[:0]
+		for _, item := range fs.fields {
+			if item != cf {
+				filtered = append(filtered, item)
+			}
+		}
+		fs.fields = filtered
+	} else {
+		fs.fields = append(fs.fields, field)
+	}
+
+	if field.Var() != "" {
+		fs.fieldsByVar[field.Var()] = field
+	}
+}
+
+func (fs *Fields) RemoveField(fvar string) *Field {
+	var removed *Field
+	cf, ok := fs.fieldsByVar[fvar]
+	if ok {
+		filtered := fs.fields[:0]
+		for _, item := range fs.fields {
+			if item != cf {
+				filtered = append(filtered, item)
+			} else {
+				removed = item
+			}
+		}
+		fs.fields = filtered
+		delete(fs.fieldsByVar, fvar)
+	}
+
+	return removed
+}
+
+func (fs *Fields) Field(fvar string) *Field {
+	cf, ok := fs.fieldsByVar[fvar]
+	if ok {
+		return cf
+	}
+	return nil
+}
+
+func (fs *Fields) All() []*Field {
+	return fs.fields
+}
+
+func (fs *Fields) Init() {
+	fs.fields = nil
+	fs.fieldsByVar = make(map[string]*Field)
+}
+
+func (fs *Fields) Clear() {
+	fs.fields = nil
+	fs.fieldsByVar = make(map[string]*Field)
+}
+
+func (fs *Fields) Contains(fvar string) bool {
+	_, ok := fs.fieldsByVar[fvar]
+	return ok
+}
+
+func (fs *Fields) RawString() string {
+	var bufferFields bytes.Buffer
+	bufferFields.WriteString("fields=[")
+	if len(fs.fields) == 0 {
+		bufferFields.WriteString("*nil*")
+	} else {
+		for _, f := range fs.fields {
+			bufferFields.WriteString(" ")
+			bufferFields.WriteString(f.RawString())
+		}
+	}
+	bufferFields.WriteString("]")
+
+	var bufferFieldsByVar bytes.Buffer
+	bufferFieldsByVar.WriteString("fieldsByVar={")
+	if len(fs.fields) == 0 {
+		bufferFieldsByVar.WriteString("*nil*")
+	} else {
+		for k, v := range fs.fieldsByVar {
+			bufferFieldsByVar.WriteString(" ")
+			bufferFieldsByVar.WriteString( k + ":")
+			bufferFieldsByVar.WriteString(v.RawString())
+		}
+	}
+	bufferFieldsByVar.WriteString("}")
+
+	return  bufferFields.String() + " " + bufferFieldsByVar.String()
+}
+
+func (fs *Fields) SimpleString() string {
+	var buffer bytes.Buffer
+	if len(fs.fields) == 0 {
+		buffer.WriteString("*nil*")
+	} else {
+		for _, f := range fs.fields {
+			buffer.WriteString(f.SimpleString())
+		}
+	}
+	return "Fields{fields=" + buffer.String() + "}"
+}
---
+++ module/form/form.go
@@ -0,0 +1,115 @@
+package form
+
+import "github.com/ortuman/jackal/xmpp"
+
+type Form struct {
+	instruction string
+	title       string
+	ftype       string
+
+	fields Fields
+}
+
+func New(ftype string, title string, instruction string) *Form {
+	return &Form{
+		instruction: instruction,
+		title:       title,
+		ftype:       ftype,
+	}
+}
+
+func NewFormFromElement(element *xmpp.Element) *Form {
+	form := &Form{}
+	form.fields.Init()
+
+	ftype := element.Attributes().Get("type")
+	if ftype != "" {
+		form.ftype = ftype
+	}
+
+	for _, itemElem := range element.Elements().All() {
+		switch itemElem.Name() {
+		case "title":
+			form.SetTitle(itemElem.Text())
+		case "instructions":
+			form.SetInstruction(itemElem.Text())
+		case "field":
+			sub, ok := itemElem.(*xmpp.Element)
+			if ok {
+				field := NewFieldFromElement(sub)
+				form.fields.AddField(field)
+			}
+		}
+	}
+
+	return form
+}
+
+func (f *Form) Element() *xmpp.Element {
+	form := xmpp.NewElementNamespace("x", "jabber:x:data")
+
+	if f.ftype != "" {
+		form.SetAttribute("type", f.ftype)
+	}
+
+	if f.title != "" {
+		newElem := xmpp.NewElementName("title")
+		newElem.SetText(f.title)
+		form.AppendElement(newElem)
+	}
+
+	if f.instruction != "" {
+		newElem := xmpp.NewElementName("instructions")
+		newElem.SetText(f.instruction)
+		form.AppendElement(newElem)
+	}
+
+	for _, fieldItem := range f.fields.All() {
+		form.AppendElement(fieldItem.Element())
+	}
+
+	return form
+}
+
+func (f *Form) Instruction() string {
+	return f.instruction
+}
+
+func (f *Form) SetInstruction(instruction string) {
+	f.instruction = instruction
+}
+
+func (f *Form) Title() string {
+	return f.title
+}
+
+func (f *Form) SetTitle(title string) {
+	f.title = title
+}
+
+func (f *Form) Type() string {
+	return f.ftype
+}
+
+func (f *Form) SetType(ftype string) {
+	f.ftype = ftype
+}
+
+func (f *Form) Contains(fvar string) bool {
+	return f.fields.Contains(fvar)
+}
+
+func (f *Form) AddField(field *Field) {
+	f.fields.AddField(field)
+}
+
+func (f *Form) RemoveField(fvar string) *Field {
+	return f.fields.RemoveField(fvar)
+}
+func (f *Form) Field(fvar string) *Field {
+	return f.fields.Field(fvar)
+}
+func (f *Form) AllFields() []*Field {
+	return f.fields.All()
+}
+
