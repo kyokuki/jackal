e74255d3ebb68a77f79a2e3a3b171a8d3dac88b7
yuqi_ryuu <yuqi_ryuu@sina.com>
2018-11-15 17:18:55 +0800

    pubsub M.

--- component/pubsub/repository/storage/sql/pubsub_user_roster.go
+++ component/pubsub/repository/storage/sql/pubsub_user_roster.go
@@ -4,13 +4,14 @@ import (
 	"github.com/ortuman/jackal/xmpp/jid"
 	"database/sql"
 	"github.com/ortuman/jackal/component/pubsub/repository/storage/model"
+	"strings"
 )
 
 func (s *Storage) GetUserRoster(owner jid.JID) ([]model.UserRosterItem, error) {
 	var err error
 
 	rows, err := s.db.Query(`
-		select username, jid, name, subscription
+		select username, jid, name, subscription, groups
 		from roster_items
 		where username = ? `, owner.Node())
 	if err != nil && err != sql.ErrNoRows {
@@ -21,10 +22,12 @@ func (s *Storage) GetUserRoster(owner jid.JID) ([]model.UserRosterItem, error) {
 	var userRosterArr []model.UserRosterItem
 	for rows.Next() {
 		var userRoster model.UserRosterItem
-		err = rows.Scan(&userRoster.Username, &userRoster.JID, &userRoster.Subscription)
+		var strGroups string
+		err = rows.Scan(&userRoster.Username, &userRoster.JID, &userRoster.Subscription, &strGroups)
 		if err != nil {
 			return nil, err
 		}
+		userRoster.Groups = strings.Split(strGroups, ";")
 		userRosterArr = append(userRosterArr, userRoster)
 	}
 	return userRosterArr, nil
